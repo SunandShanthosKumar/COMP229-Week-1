dd4dad34435bdbf695e88e221855523f3dc32a21
Sunand Shanthos Kumar <sunandmasters@gmail.com>
2023-01-16 09:02:35 -0500

    initial commit

---
+++ arrow.js
@@ -0,0 +1,17 @@
+/*function sum1(a,b){
+    return a+b;
+}
+let result=sum1(3,4);
+console.log(result);    
+*/
+
+/*const sum2=(a,b) => {
+    return a+b;
+}
+let result=sum2(4,6);
+console.log(result);
+*/
+
+const sum2=(a,b) => a+b;
+let result=sum2(2,6);
+console.log(result);
---
+++ main.js
@@ -0,0 +1,13 @@
+/*import {halfof, multiply} from './lib.js';
+console.log(halfof(138));
+console.log(multiply(210,2));
+*/
+
+/*import doSomething from './doSomething.js';
+doSomething();
+*/
+
+import { flag, touch } from './validator.js';
+console.log(flag);
+touch();
+console.log(flag);
