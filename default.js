53177ac3edd4a78e72468bce8164493040d6bda8
Sunand Shanthos Kumar <sunandmasters@gmail.com>
2023-01-16 17:48:00 -0500

    initial commit

---
+++ default.js
@@ -0,0 +1,11 @@
+/*import { flag, touch } from './validator.js';
+console.log(flag);
+touch();
+console.log(flag);
+*/
+
+function add(x, y = 0) {
+    return x + y;
+    }
+    add(3);
+    add(3,4);
