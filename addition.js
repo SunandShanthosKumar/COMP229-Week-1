2e0dfbb35fd81a8920f49c42e788207046230ea7
Sunand Shanthos Kumar <sunandmasters@gmail.com>
2023-01-16 09:28:44 -0500

    initial commit

---
+++ addition.js
@@ -0,0 +1,5 @@
+function sum1(a,b){
+    return a+b;
+}
+let result=sum1(3,4);
+console.log(result);    
--- arrow.js
+++ arrow.js
@@ -11,7 +11,15 @@ console.log(result);
 let result=sum2(4,6);
 console.log(result);
 */
-
 const sum2=(a,b) => a+b;
 let result=sum2(2,6);
 console.log(result);
+
+/*const author = {
+    fullName: "Bob Alice",
+    books: [],
+    printBooks() {
+    this.books.forEach(book => console.log(book + 'by ' +this.fullName));
+    }
+};
+*/
